01bbbafaa46a52d0e90322af7d5cc00a8d5ef20a
Vahid-Soudagar <mohammadvahidsoudagar264@gmail.com>
2023-12-16 22:30:45 +0530

    commit 1

---
+++ src/main/java/com/driver/models/User.java
@@ -0,0 +1,85 @@
+package com.driver.models;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Blog> blogList = new ArrayList<>();
+
+    public User(Integer id, String firstName, String lastName, String username, String password, List<Blog> blogList) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.blogList = blogList;
+    }
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Blog> getBlogList() {
+        return blogList;
+    }
+
+    public void setBlogList(List<Blog> blogList) {
+        this.blogList = blogList;
+    }
+}
